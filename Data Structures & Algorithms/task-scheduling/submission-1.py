03ceb8251c304aac9a214633e834b502f1fc0c1b
MichelangeloOlivieri <michelangelo.olivieri00@gmail.com>
2026-04-23 12:02:59 +0200

    Add: task-scheduling - submission-1

---
+++ Data Structures & Algorithms/task-scheduling/submission-1.py
@@ -0,0 +1,33 @@
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+
+        """
+        1) No empty input; example on paper
+        2) Find the most frequent tasks; use a heap to order based on frequency
+        """
+
+        count = Counter(tasks)
+        max_heap = [-c for c in count.values()]
+        heapq.heapify(max_heap)
+
+        time = 0
+        q = deque()
+
+        while max_heap or q:
+            time += 1
+
+            if max_heap:
+                cnt = heapq.heappop(max_heap) + 1
+                if cnt != 0:
+                    q.append([cnt, time + n])
+
+            if q and q[0][1] == time:
+                cnt = q.popleft()[0]
+                heapq.heappush(max_heap, cnt)
+
+        return time
+
+        """
+        3) Ok
+        4) Time complexity O(n), where n = len(tasks), space complexity O(1)
+        """
