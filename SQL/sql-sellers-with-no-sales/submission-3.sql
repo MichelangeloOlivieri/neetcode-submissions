4aa50352babac5a76fcc5606f51bd635ebcc8940
MichelangeloOlivieri <michelangelo.olivieri00@gmail.com>
2026-04-23 17:58:02 +0200

    Add: sql-sellers-with-no-sales - submission-3

---
+++ SQL/sql-sellers-with-no-sales/submission-3.sql
@@ -0,0 +1,16 @@
+-- Write your query below
+
+/*
+1) No duplicates as there is a primary key; there could be NULLs
+2) Maybe even without JOINs
+*/
+
+SELECT s.seller_name
+FROM seller AS s
+WHERE NOT EXISTS
+(
+    SELECT 1
+    FROM orders AS o
+    WHERE s.seller_id = o.seller_id AND EXTRACT(YEAR FROM o.sale_date) = 2020
+)
+ORDER BY s.seller_name ASC
