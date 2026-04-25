4ee2485dacee0c9681574192e3beee7e140c7fca
MichelangeloOlivieri <michelangelo.olivieri00@gmail.com>
2026-04-25 19:21:33 +0200

    Add: combination-target-sum - submission-5

---
+++ Data Structures & Algorithms/combination-target-sum/submission-5.py
@@ -0,0 +1,32 @@
+class Solution:
+    def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
+        
+        """
+        1) Empty input; example: nums = [5, 2, 3], target = 10 -> 4 combinations
+        2) - Solution 1: a brute force approach might be done with a dfs on a binary
+        tree
+        """
+
+        res = []
+
+        def dfs(i, cur, total):
+            if total == target:
+                res.append(cur.copy())
+                return
+            if total > target or i >= len(nums):
+                return
+
+            cur.append(nums[i])
+            dfs(i, cur, total + nums[i])
+            cur.pop()
+            dfs(i + 1, cur, total)
+
+        dfs(0, [], 0)
+        return res
+
+        """
+        3) Syntax and dry run: ok
+        4) Time complexity O(2^(target/min(nums) + len(nums))); space complexity 
+        O(2^(target/min(nums)))
+        """
+
