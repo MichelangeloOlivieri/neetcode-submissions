37b3ec5e557fd1ab3431d7dba347b46056ed3083
MichelangeloOlivieri <michelangelo.olivieri00@gmail.com>
2026-05-01 12:31:52 +0200

    Add: combinations-of-a-phone-number - submission-0

---
+++ Data Structures & Algorithms/combinations-of-a-phone-number/submission-0.py
@@ -0,0 +1,40 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        
+        """
+        1) Example on paper
+        2) Dfs
+        """
+
+        if not digits:
+            return []
+
+        res = []
+        dictionary = {
+            "2" : {"a", "b", "c"}, 
+            "3" : {"d", "e", "f"},
+            "4" : {"g", "h", "i"},
+            "5" : {"j", "k", "l"},
+            "6" : {"m", "n", "o"},
+            "7" : {"p", "q", "r", "s"},
+            "8" : {"t", "u", "v"},
+            "9" : {"w", "x", "y", "z"}
+        }
+
+        def dfs(i, cur):
+            if i > len(digits) - 1:
+                res.append(cur)
+                return
+
+            for c in dictionary[digits[i]]:
+                cur += c
+                dfs(i + 1, cur)
+                cur = cur[: -1]
+
+        dfs(0, "")
+        return res
+
+        """
+        3) Ok
+        4) Time complexity O(4^n), where n = len(digits); space complexity O(n)
+        """
