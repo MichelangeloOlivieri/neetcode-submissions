2f5a1333ad7947ebf1cfcb772301c825e32bcf01
MichelangeloOlivieri <michelangelo.olivieri00@gmail.com>
2026-05-07 10:52:18 +0200

    Add: surrounded-regions - submission-3

---
+++ Data Structures & Algorithms/surrounded-regions/submission-3.py
@@ -0,0 +1,57 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+
+        """
+        1) Empty input; example seen; squares on edge never considered surrounded
+        2) First bfs starting at the edge squares where you have 'O', then double for
+        cycle
+        """
+
+        if not board: 
+            return
+
+        m = len(board)
+        n = len(board[0])
+        directions = [[1, 0], [0, 1], [-1, 0], [0, -1]]
+
+        def bfs(i, j):
+            board[i][j] = 'Y'
+            q = collections.deque() # this belongs HERE
+            q.append((i, j))
+
+            while q:
+
+                row, col = q.popleft()                
+                for dr, dc in directions:
+                    new_row = row + dr
+                    new_col = col + dc
+
+                    if (new_row in range(m)
+                    and new_col in range(n)
+                    and board[new_row][new_col] == 'O'):
+                        board[new_row][new_col] = 'Y'
+                        q.append((new_row, new_col))
+
+        for i in range(m):
+            if board[i][0] == 'O':
+                bfs(i, 0)
+            if board[i][n - 1] == 'O':
+                bfs(i, n - 1)     
+
+        for j in range(n):
+            if board[0][j] == 'O':
+                bfs(0, j)
+            if board[m - 1][j] == 'O':
+                bfs(m - 1, j)  
+
+        for i in range(m):
+            for j in range(n):
+                if board[i][j] == 'O':
+                    board[i][j] = 'X'
+                if board[i][j] == 'Y':
+                    board[i][j] = 'O' 
+
+        """
+        3) Ok
+        4) Time complexity O(m * n); space complexity O(m * n)
+        """
