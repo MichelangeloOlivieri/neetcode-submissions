dee75b8d693e28ee15bc6bd9413fa6ee31e519e9
MichelangeloOlivieri <michelangelo.olivieri00@gmail.com>
2026-05-08 11:13:33 +0200

    Add: course-schedule-ii - submission-3

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-3.py
@@ -0,0 +1,50 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+
+        """
+        1) Empty input
+        2) It is a graph problem
+        """
+
+        from typing import List
+import collections
+
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        
+        graph = collections.defaultdict(list)
+        for i, nei in prerequisites:
+            graph[i].append(nei)
+
+        res = []
+        visited = set()
+        visiting = set()
+
+        def dfs(i):
+            if i in visiting:
+                return True
+            if i in visited:
+                return 
+
+            visiting.add(i)
+            for nei in graph[i]:
+                if dfs(nei):
+                    return True
+            
+            visiting.remove(i)
+            visited.add(i)
+            res.append(i)
+
+        for n in range(numCourses):
+            if dfs(n):
+                return []
+
+        return res
+
+        """
+        3) Ok
+        4) Time complexity O(numCourses); space complexity O(numCourses)
+        """
+
+        
+        
