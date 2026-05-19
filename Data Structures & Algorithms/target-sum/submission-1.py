87f560106fca8d44e9edd779a5300b50622de274
MichelangeloOlivieri <michelangelo.olivieri00@gmail.com>
2026-05-19 11:01:51 +0200

    Add: target-sum - submission-1

---
+++ Data Structures & Algorithms/target-sum/submission-1.py
@@ -0,0 +1,35 @@
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        
+        """
+        1) Empty input; example seen; I understand you have a binary choice for each
+        integer: ie you cannot sum or subtract an integer multiple times
+        2) Dfs with memoization
+        """
+
+        if not nums:
+            return 0
+
+        memo = {}
+
+        def dfs(i, total):
+            if i == len(nums):
+                if total == target:
+                    return 1
+                else:
+                    return 0
+            if (i, total) in memo:
+                return memo[(i, total)]
+
+            sum_current = dfs(i + 1, total + nums[i])
+            subtract_current = dfs(i + 1, total - nums[i])
+            memo[(i, total)] = sum_current + subtract_current
+
+            return memo[(i, total)]
+
+        return dfs(0, 0)
+
+        """
+        3) Ok
+        4) Time complexity O(n); space complexity O(n)
+        """
