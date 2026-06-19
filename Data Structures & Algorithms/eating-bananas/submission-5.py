22d2342a4707bc4a751ac94be08581fd4ff6732d
MichelangeloOlivieri <michelangelo.olivieri00@gmail.com>
2026-06-19 19:10:31 +0200

    Add: eating-bananas - submission-5

---
+++ Data Structures & Algorithms/eating-bananas/submission-5.py
@@ -0,0 +1,27 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+
+        l = 0
+        r = max(piles)
+
+        if not piles or r == 0:
+            return 0
+
+        while l <= r:
+            mid = (l + r) // 2
+            if mid != 0:
+                hours = sum((p - 1) // mid + 1 for p in piles)
+            else:
+                hours = float('inf')
+
+            if hours > h:
+                l = mid + 1
+            else:
+                r = mid - 1
+
+        return l
+
+        """
+        Time complexity O(n * log(h)); space complexity O(1)
+        """
+        
