fbe3dfff3d636d618e5e51f8022dba53645879a7
MichelangeloOlivieri <michelangelo.olivieri00@gmail.com>
2026-07-15 12:45:55 +0200

    Add: validate-parentheses - submission-6

---
+++ Data Structures & Algorithms/validate-parentheses/submission-6.py
@@ -0,0 +1,28 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+
+        """
+        1) Only two options: "(())" or "()()" (and their variants with other types of
+        parenthesis)
+        2) Graph to distinguish parenthesis types, and a stack to process the whole 
+        thing
+        """
+
+        if not s:
+            return False
+        
+        graph = {"(" : ")", "[" : "]", "{" : "}"}
+        stack = []
+
+        for c in s:
+            if c in graph:
+                stack.append(c)
+            elif stack and c != graph[stack[-1]]:
+                return False
+            elif stack:
+                stack.pop()
+            else:
+                return False
+
+        return not stack
+
