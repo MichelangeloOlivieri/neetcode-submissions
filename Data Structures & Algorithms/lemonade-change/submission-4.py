b45405ef49e160fa6e363869547f13890e96473c
MichelangeloOlivieri <michelangelo.olivieri00@gmail.com>
2026-08-23 11:06:14 +0200

    Add: lemonade-change - submission-4

---
+++ Data Structures & Algorithms/lemonade-change/submission-4.py
@@ -0,0 +1,25 @@
+class Solution:
+    def lemonadeChange(self, bills: list[int]) -> bool:
+        five = 0
+        ten = 0
+        
+        for bill in bills:
+            if bill == 5:
+                five += 1
+            elif bill == 10:
+                five -= 1
+                ten += 1
+            elif ten > 0:
+                ten -= 1
+                five -= 1
+            else:
+                five -= 3
+
+            if five < 0:
+                return False
+                
+        return True
+
+        """
+        Time complexity O(n), where n = len(bills); space complexity O(1)
+        """
