fcf0bd09fb2f73a4bfc92907dfaa51e8f4774323
MichelangeloOlivieri <michelangelo.olivieri00@gmail.com>
2026-09-06 11:17:28 +0200

    Add: jump-game-vii - submission-4

---
+++ Data Structures & Algorithms/jump-game-vii/submission-4.py
@@ -0,0 +1,29 @@
+class Solution:
+    def canReach(self, s: str, minJump: int, maxJump: int) -> bool:
+
+        if not s or s[-1] == "1":
+            return False
+
+        q = deque([0])
+        furthest = 0
+
+        while q:
+            i = q.popleft()
+
+            start = max(i + minJump, furthest + 1)
+            end = min(i + maxJump, len(s) - 1)
+
+            for j in range(start, end + 1):
+                if s[j] == "0":
+                    if j == len(s) - 1:
+                        return True
+                    q.append(j)
+
+            furthest = end    
+
+        return False
+
+        """
+        - Time complexity O(n), where n = len(s)
+        - Space complexity O(n)
+        """
