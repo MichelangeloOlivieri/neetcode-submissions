e98248e0f4435531c8ebc40737a403b2f593ea37
MichelangeloOlivieri <michelangelo.olivieri00@gmail.com>
2026-09-15 20:30:27 +0200

    Add: intersection-of-two-arrays - submission-1

---
+++ Data Structures & Algorithms/intersection-of-two-arrays/submission-1.py
@@ -0,0 +1,22 @@
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+
+        if not nums1:
+            return []
+        if not nums2:
+            return []
+
+        first = set(nums1)
+        second = set(nums2)
+        res = []
+
+        for n in first:
+            if n in second:
+                res.append(n)
+
+        return res
+
+        """
+        - Time complexity O(m + n), where m = len(nums1) and n = len(nums2)
+        - Space complexity O(m + n)
+        """
