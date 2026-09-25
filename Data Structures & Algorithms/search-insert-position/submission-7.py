a18bf889b87273ca721dfde5488848f719fa6c2b
MichelangeloOlivieri <michelangelo.olivieri00@gmail.com>
2026-09-25 10:10:35 +0200

    Add: search-insert-position - submission-7

---
+++ Data Structures & Algorithms/search-insert-position/submission-7.py
@@ -0,0 +1,24 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+
+        if not nums:
+            return 0
+
+        l = 0
+        r = len(nums) - 1
+
+        while l <= r:
+            mid = l + (r - l) // 2
+            if target > nums[mid]:
+                l = mid + 1
+            elif target < nums[mid]:
+                r = mid - 1
+            else:
+                return mid
+
+        return l
+
+        """
+        - Time complexity O(log(n)), where n = len(nums)
+        - Space complexity O(1)
+        """
